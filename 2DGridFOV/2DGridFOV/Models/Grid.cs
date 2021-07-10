3bc201f7b0c1276486a6ee45a1c73634d40fb4dd
A.Dopham <alexandre34980@outlook.fr>
2021-07-10 13:20:40 +0200

    Initialization

---
+++ 2DGridFOV/2DGridFOV/Models/Grid.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2DGridFOV.Models
+{
+    public class Grid<T> : IGrid
+    {
+        private T[,] _content;
+        public T[,] Content
+        {
+            get
+            {
+                return this._content;
+            }
+        }
+
+        public Grid(int size)
+        {
+            this._content = new T[size, size];
+        }
+    }
+}
---
+++ 2DGridFOV/2DGridFOV/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2DGridFOV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
